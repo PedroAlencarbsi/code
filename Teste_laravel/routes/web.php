ac27c84f925bd923db47008ca3177c144c4ebcde
PedroAlencarbsi <pedro.alencar@edu.unirio.br>
2022-07-05 19:06:47 -0300

    autenticaçao de usuario

--- Teste_laravel/routes/web.php
+++ Teste_laravel/routes/web.php
@@ -22,3 +22,13 @@ Route::get('/events/{id}', [EventController::class, 'show']);
 
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
