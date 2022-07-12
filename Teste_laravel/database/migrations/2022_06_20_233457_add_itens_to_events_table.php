f3cc682bcddd87e9d453854e315d3438d429a24b
PedroAlencarbsi <pedro.alencar@edu.unirio.br>
2022-07-12 20:41:19 -0300

    migration correta

--- Teste_laravel/database/migrations/2022_06_20_233457_add_itens_to_events_table.php
+++ Teste_laravel/database/migrations/2022_06_20_233457_add_itens_to_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddItensToEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,7 +27,6 @@ return new class extends Migration
     {
         Schema::table('events', function (Blueprint $table) {
             $table->dropColumn('itens');
-        
         });
     }
 };
